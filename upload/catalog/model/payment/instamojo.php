8c3e02c4c2bedb03192db68a0e78d21cfdf72cb5
pavan <pism198@gmail.com>
2017-08-17 11:32:35 +0530

    Fixed in last commit for geo zone

--- upload/catalog/model/payment/instamojo.php
+++ upload/catalog/model/payment/instamojo.php
@@ -2,11 +2,10 @@
 class ModelPaymentInstamojo extends Model {
   public function getMethod($address, $total) {
     $this->load->language('payment/instamojo');
-    $this->load->model('setting/setting');
 
     $checkout_label = $this->config->get('instamojo_checkout_label');
     
-    $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('cod_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+    $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('instamojo_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
     
     if (!$this->config->get('instamojo_geo_zone_id')) {
       $status = true;
